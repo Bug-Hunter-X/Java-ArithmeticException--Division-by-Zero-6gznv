f881bd8899e59b3ebd3d47a4667d977b0a4a846d
BugHunter-X <askzenai@gmail.com>
2025-02-11 01:14:04 +0100

    Add Bug.java file

---
+++ Bug.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 0;
+        int result = i / j; // This line will throw an ArithmeticException
+        System.out.println(result);
+    }
+}
